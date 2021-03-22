5b7a826377ccbd96d43a84f00995b194600d5083
Nikolas Haug <nikohaug21@gmail.com>
2021-03-22 15:43:11 -0500

    add base components and style files

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,10 +1,34 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
+require('dotenv').config();
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: 'Curio Shoppe',
+    author: 'Nikolas Haug',
+    description: 'A simple e-commerce shoppe built with gatsby, sanity, and snipcart',
+    email: 'nikolas@nikolashaug.com',
+    // siteUrl: ''
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    // {
+    //   resolve: `gatsby-source-sanity`,
+    //   options: {
+    //     projectId: '',
+    //     dataset: 'production',
+    //     watchMode: true,
+    //     token: process.env.SANITY_TOKEN
+    //   }
+    // },
+    // {
+    //   resolve: `gatsby-plugin-snipcart-advanced`,
+    //   options: {
+    //     version: '3.0.29',
+    //     publicApiKey: '',
+    //     defaultLang: 'en',
+    //     currencey: 'usd',
+    //     openCartOnAdd: true,
+    //     useSideCart: true
+    //   }
+    // }
+  ],
 }

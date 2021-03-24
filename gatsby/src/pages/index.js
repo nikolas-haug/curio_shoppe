93feced1c7f786e888deef9d5441fb150cef76a3
Nikolas Haug <nikohaug21@gmail.com>
2021-03-24 15:29:54 -0500

    add grids to styles folder

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,19 +1,20 @@
-import React from "react"
-import Main from "../components/Main"
+import React from "react";
+import Main from "../components/Main";
+import { ProductGrid } from '../styles/Grids';
 
 export default function Home() {
   return (
     <>
       <Main>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, porro mollitia. Obcaecati, ducimus, esse consequatur ut cumque commodi unde autem alias dolorum assumenda in eaque cum voluptas minima et laudantium.</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, porro mollitia. Obcaecati, ducimus, esse consequatur ut cumque commodi unde autem alias dolorum assumenda in eaque cum voluptas minima et laudantium.</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, porro mollitia. Obcaecati, ducimus, esse consequatur ut cumque commodi unde autem alias dolorum assumenda in eaque cum voluptas minima et laudantium.</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, porro mollitia. Obcaecati, ducimus, esse consequatur ut cumque commodi unde autem alias dolorum assumenda in eaque cum voluptas minima et laudantium.</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, porro mollitia. Obcaecati, ducimus, esse consequatur ut cumque commodi unde autem alias dolorum assumenda in eaque cum voluptas minima et laudantium.</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, porro mollitia. Obcaecati, ducimus, esse consequatur ut cumque commodi unde autem alias dolorum assumenda in eaque cum voluptas minima et laudantium.</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, porro mollitia. Obcaecati, ducimus, esse consequatur ut cumque commodi unde autem alias dolorum assumenda in eaque cum voluptas minima et laudantium.</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, porro mollitia. Obcaecati, ducimus, esse consequatur ut cumque commodi unde autem alias dolorum assumenda in eaque cum voluptas minima et laudantium.</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, porro mollitia. Obcaecati, ducimus, esse consequatur ut cumque commodi unde autem alias dolorum assumenda in eaque cum voluptas minima et laudantium.</p>
+        <ProductGrid>
+          <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima alias placeat fuga ab natus, reiciendis illo incidunt. Unde commodi ducimus voluptatum aut, veniam voluptates dolore cum accusantium officia modi quae facilis? Dolorem, vel aliquam? Aliquid, ab. Alias, quibusdam nostrum. Veniam sint vero laborum nesciunt mollitia nam quibusdam labore repellendus placeat?</div>
+          <div>Animi hic, ullam nobis iusto delectus cupiditate molestiae ut aliquam illo alias perspiciatis omnis deleniti adipisci dolore voluptatibus nostrum velit! Expedita maxime explicabo quo accusamus asperiores dignissimos sint, dicta at alias quibusdam doloribus temporibus voluptate tempora iusto rerum enim hic autem consequuntur. Voluptates in eius harum? Quam sint at voluptatem!</div>
+          <div>Architecto nihil totam saepe aliquid vel repellendus ut atque modi? Neque possimus velit id incidunt unde ut quas officiis quod! Libero nesciunt quas similique reprehenderit deserunt distinctio minus voluptate veritatis animi laudantium? Fugiat eum, voluptatem, blanditiis commodi earum ea non ipsa eligendi nulla a est perspiciatis ipsam natus delectus! Voluptatum.</div>
+          <div>Nesciunt praesentium obcaecati accusantium eum magnam iusto commodi deleniti rem perspiciatis, voluptatem nulla! Itaque alias quis repellat at cumque tempore nostrum praesentium earum quo! Eveniet earum maxime nobis, commodi inventore aperiam ullam. Debitis hic quidem neque saepe adipisci? Odio corporis maiores iusto animi molestiae iure esse nemo impedit fuga aut.</div>
+          <div>Modi maiores minima similique inventore, pariatur voluptatem accusamus aliquid fuga rem nisi repudiandae quis rerum, expedita voluptatum. Eum earum hic, sed eos inventore incidunt fugiat ut? Maxime nisi harum deserunt et quam modi magnam asperiores nobis deleniti corporis quisquam itaque laborum commodi laudantium voluptatum totam incidunt, odio illo tenetur perspiciatis?</div>
+          <div>Dolores hic magnam ipsa corrupti rem doloribus nobis dolorum, alias ut, sit sint aliquid ea est dolorem iure vitae. Commodi dolores, optio esse ad inventore necessitatibus quis, dolore dolorum at, ea quasi! Recusandae, a illum sapiente earum quo porro possimus expedita. Quod, laudantium ut asperiores harum excepturi id assumenda beatae.</div>
+          <div>Impedit praesentium doloremque sed architecto error iste ut veniam, velit cupiditate, in accusamus commodi asperiores facilis perspiciatis, officiis dicta ullam magni! Minus, sint ut. Eligendi, doloremque quam! Provident nihil laborum vero perferendis sint odit enim tempore sit explicabo, saepe minima suscipit culpa quo! Asperiores neque eveniet doloribus maxime itaque provident?</div>
+        </ProductGrid>
       </Main>
     </>
   )

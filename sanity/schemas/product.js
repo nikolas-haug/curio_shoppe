e36d86aba7d84af1d29192f7d154b0a57fecd27e
Nikolas Haug <nikohaug21@gmail.com>
2021-03-25 21:05:46 -0500

    add sanity to project

---
+++ sanity/schemas/product.js
@@ -0,0 +1,13 @@
+export default {
+    name: 'product',
+    title: 'Products',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Product name',
+            type: 'string',
+            description: 'Name of the product',
+        }
+    ]
+}

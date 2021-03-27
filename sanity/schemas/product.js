075e56eb3dc052a73802c6e5079b1ed7b017c51a
Nikolas Haug <nikohaug21@gmail.com>
2021-03-26 19:27:36 -0500

    add to product schema

--- sanity/schemas/product.js
+++ sanity/schemas/product.js
@@ -8,6 +8,32 @@ export default {
             title: 'Product name',
             type: 'string',
             description: 'Name of the product',
-        }
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'name',
+                maxLength: 100,
+            },
+            validation: Rule => Rule.required()
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+            validation: Rule => Rule.required()
+        },
+        {
+            name: 'description',
+            title: 'Product Description',
+            type: 'string',
+            description: 'Description of the product',
+            validation: Rule => Rule.required()
+        },
     ]
 }
